5295d6d58b711489aa6456d6e0a351b79b5c2267
sami2515 <samikhan031027@gmail.com>
2026-05-11 01:28:41 +0500

    4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,12 @@ export const metadata: Metadata = {
   creator: SITE_NAME,
   publisher: SITE_NAME,
   icons: {
-    icon: [{ url: "/favicon.png", type: "image/png", sizes: "500x500" }],
-    shortcut: ["/favicon.png"],
-    apple: [{ url: "/favicon.png", type: "image/png", sizes: "500x500" }],
+    icon: [
+      { url: "/favicon.ico?v=2", sizes: "any" },
+      { url: "/favicon.png?v=2", type: "image/png", sizes: "500x500" }
+    ],
+    shortcut: ["/favicon.png?v=2"],
+    apple: [{ url: "/favicon.png?v=2", type: "image/png", sizes: "500x500" }],
   },
   verification: {
     google: "4-XCsUuYDKGiUs32T-pvjgzRXVSvsQP4wF0aHh_CuKU",
